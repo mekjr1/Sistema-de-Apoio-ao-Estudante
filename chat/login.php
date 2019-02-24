b9f13fa35af67bf19c0643b6d1e50b42c01d2e20
mekjr1 <gafar_meque@hotmail.com>
2019-02-24 20:27:25 +0200

    first push to github

---
+++ chat/login.php
@@ -0,0 +1,117 @@
+<!--
+//login.php
+!-->
+
+<?php
+
+include('database_connection.php');
+
+session_start();
+
+$message = '';
+
+if(isset($_SESSION['user_id']))
+{
+	header('location:index.php');
+}
+
+if(isset($_POST['login']))
+{
+	$query = "
+		SELECT * FROM login 
+  		WHERE username = :username
+	";
+	$statement = $connect->prepare($query);
+	$statement->execute(
+		array(
+			':username' => $_POST["username"]
+		)
+	);	
+	$count = $statement->rowCount();
+	if($count > 0)
+	{
+		$result = $statement->fetchAll();
+		foreach($result as $row)
+		{
+			if(password_verify($_POST["password"], $row["password"]))
+			{
+				$_SESSION['user_id'] = $row['user_id'];
+				$_SESSION['username'] = $row['username'];
+				$sub_query = "
+				INSERT INTO login_details 
+	     		(user_id) 
+	     		VALUES ('".$row['user_id']."')
+				";
+				$statement = $connect->prepare($sub_query);
+				$statement->execute();
+				$_SESSION['login_details_id'] = $connect->lastInsertId();
+				header('location:index.php');
+			}
+			else
+			{
+				$message = '<label>Wrong Password</label>';
+			}
+		}
+	}
+	else
+	{
+		$message = '<label>Wrong Username</labe>';
+	}
+}
+
+
+?>
+
+<html>  
+    <head>  
+        <title>Chat</title>  
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    </head>  
+    <body>  
+        <div class="container">
+			<br />
+			
+			<h3 align="center">Entrada para o Chat</h3><br />
+			<br />
+			<div class="panel panel-default">
+  				<div class="panel-heading"><b>Chat-SAE</b></div>
+				<div class="panel-body">
+					<p class="text-danger"><?php echo $message; ?></p>
+					<form method="post">
+						<div class="form-group">
+							<label>Enter Username</label>
+							<input type="text" name="username" class="form-control" required />
+						</div>
+						<div class="form-group">
+							<label>Enter Password</label>
+							<input type="password" name="password" class="form-control" required />
+						</div><br><br>
+						<div class="form-group" align="center">
+							<input type="submit" name="login" class="btn btn-info" value="Iniciar Sessão" />
+						</div>
+						
+						<p>Ainda não tem uma conta? Registre Aqui</p> 
+						<button type="submit"><a href="register.php">Registro</a></button>
+						<button type="submit"><a href="http://localhost/SAE/Chat.html">Voltar</a></button>
+					</form>
+					<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+					<!-- webslesson_mainblogsec_Blog1_1x1_as -->
+					<ins class="adsbygoogle"
+						 style=""
+						 data-ad-client="ca-pub-4529508631166774"
+						 data-ad-host="ca-host-pub-1556223355139109"
+						 data-ad-host-channel="L0007"
+						 data-ad-slot="6573078845"
+						 data-ad-format="auto"></ins>
+					<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+					</script>
+				</div>
+			</div>
+		</div>
+
+    </body>  
+</html>
